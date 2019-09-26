3c0ee326a27cc0688ae8e62c25e8d7c0f1f17de4
Matthew Bodily <mattcbodily@gmail.com>
2019-09-25 23:30:52 -0600

    added landing component, and conditional rendering for the header, footer, and side menu so they don't show up during when the landing page is on the screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import {HashRouter} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import Header from './Components/Header/Header';
 import SideMenu from './Components/SideMenu/SideMenu';
 import Footer from './Components/Footer/Footer';
 import routes from './routes';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <HashRouter>
       <div className="App">
-        <Header />
-        <SideMenu />
-        {routes}
-        <Footer />
+        {props.match.url === '/'
+        ?(
+          <>
+            {routes}
+          </>
+        )
+        :(<>
+            <Header />
+            <SideMenu />
+            {routes}
+            <Footer />
+          </>)}
       </div>
-    </HashRouter>
   );
 }
 
-export default App;
+export default withRouter(App);
