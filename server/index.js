e8bf6bf59e8db63e4e68d1bc522f58d327cee052
Matthew Bodily <mattcbodily@gmail.com>
2019-10-04 12:52:45 -0600

    added dotenv massive and express-session. DB now connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
-const express = require('express');
-const app = express();
+require('dotenv').config();
+const express = require('express'),
+      massive = require('massive'),
+      session = require('express-session'),
+      {SERVER_PORT, CONNECTION_STRING} = process.env,
+      app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(4040, () => console.log('Server running on 4040'));
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+})
+
+const port = SERVER_PORT || 4040;
+app.listen(port, () => console.log(`Coding the things on ${port}`));
